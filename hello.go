c72e320bac3294b8d149c76c143db32f64141df2
H. Dale McBane <harold.d.mcbane@navy.mil>
2017-05-09 08:47:24 -0400

    Revert hello...

--- hello.go
+++ hello.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	var list [100]int64
-	var sides = int64(20)
-	rand.Seed(time.Now().UnixNano())
-	for i := range list {
-		list[i] = rand.Int63n(sides)
-	}
-	fmt.Printf("%+v\n", list)
+	fmt.Printf("Hello, World!")
 }

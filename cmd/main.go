66e13af6b799b09a5e77c173c03e032a85ae205d
H. Dale McBane <harold.d.mcbane@navy.mil>
2017-05-17 16:34:49 -0400

    Move main and parameters to the cmd folder...

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dmcbane/dierollergo/pkg"
+)
 
 func main() {
 	var dice, sides, modifier, keep, iterations, verbose = GetParameters()
 
-	dieroll := NewDieRoll(*dice, *sides, *modifier, *keep)
+	dieroll := dieroller.NewDieRoll(*dice, *sides, *modifier, *keep)
 
 	for i := uint32(0); i < *iterations; i++ {
 		dieroll.Roll()

4e9c0c632640cb15b986d4c1b72b4988c75f4221
H. Dale McBane <h.dale.mcbane@gmail.com>
2017-05-29 11:22:30 -0400

    Completed pathfindercharacter console application...
    use the generators created by the GetGenerator method...

--- pathfindercharacter/main.go
+++ pathfindercharacter/main.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"fmt"
-	// 	"github.com/dmcbane/dierollergo/dierollerpkg"
 )
 
 func main() {
 	gentype, options, number, verbose := GetParameters()
-	for i := uint32(0); i < *number; i++ {
-		fmt.Printf("%s - %s - %s\n", gentype, options, verbose)
+	fn := gentype.GetGenerator(options)
+	if verbose {
+		fmt.Printf("gentype: %v\noptions: %v\n", gentype, options)
 	}
 
-	// dieroll := dierollerpkg.NewDieRoll(*dice, *sides, *modifier, *keep)
-
-	// for i := uint32(0); i < *iterations; i++ {
-	// 	dieroll.Roll()
-	// 	if *verbose {
-	// 		fmt.Printf("%s => %d\n", dieroll.StandardStringVerbose(), dieroll.LastRoll().Result)
-	// 	} else {
-	// 		fmt.Printf("%d\n", dieroll.LastRoll().Result)
-	// 	}
-	// }
+	for i := uint32(0); i < number; i++ {
+		if verbose {
+			cma := fn()
+			fmt.Printf("%v c: %v m: %v\n", cma.Abils, cma.Cost, cma.Modifier)
+		} else {
+			fmt.Println(fn().Abils)
+		}
+	}
 }
--- pathfindercharacter/parameters.go
+++ pathfindercharacter/parameters.go
@@ -1,44 +1,64 @@
 package main
 
 import (
-	// "github.com/dmcbane/dierollergo/dierollerpkg"
+	"github.com/dmcbane/dierollergo/pathfinderpkg"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
-	// "regexp"
 )
 
-func GetParameters() (*GenerationType, *string, *uint32, *bool) {
+func GetParameters() (pathfinderpkg.GenerationType, string, uint32, bool) {
 	var (
-		pathfinder   = kingpin.New("pathfinder", "Generate Pathfinder character abilities.")
-		classic      = pathfinder.Flag("classic", "The classic method: 3D6 per ability.").Default("false").Short('c').Bool()
-		standard     = pathfinder.Flag("standard", "The standard method: 4D6 keep high 3 per ability. (this is the default)").Default("false").Short('s').Bool()
-		heroic       = pathfinder.Flag("heroic", "The heroic method: 2D6 plus 6 per ability.").Default("false").Short('r').Bool()
-		pool         = pathfinder.Flag("pool", "The pool method: 24D6 for all 6 abilities. The parameter specifies how many dice are assigned to each ability as follows: 3/3/3/3/3/9 with a minimum of 3 dice per ability.").Default("").Short('l').String()
-		purchaseType = pathfinder.Flag("purchase", "The purchase method: parameters are set according to cost. The parameter specifies the purchase type as follows: low, standard, high, and epic fantasy which provides 10, 15, 20, and 25 purchase points respectively.").Default("").Short('p').String()
-		number       = pathfinder.Flag("number", "Number of characters to roll. Must be greater than 0.").Default("1").Short('n').Uint32()
-		verbose      = pathfinder.Flag("verbose", "Display additional information.").Default("false").Short('v').Bool()
+		pathfinder = kingpin.New("pathfinder", "Generate Pathfinder character abilities.")
+		classic    = pathfinder.Flag("classic", "The classic method: 3D6 per ability.").Default("false").Short('c').Bool()
+		standard   = pathfinder.Flag("standard", "The standard method: 4D6 keep high 3 per ability. (this is the default)").Default("false").Short('s').Bool()
+		heroic     = pathfinder.Flag("heroic", "The heroic method: 2D6 plus 6 per ability.").Default("false").Short('r').Bool()
+		pool       = pathfinder.Flag("pool", "The pool method: 24D6 for all 6 abilities. The parameter specifies how many dice are assigned to each ability as follows: 3/3/3/3/3/9 with a minimum of 3 dice per ability.").Default("").Short('l').String()
+		purchase   = pathfinder.Flag("purchase", "The purchase method: parameters are set according to cost. The parameter specifies the purchase type as follows: low, standard, high, and epic fantasy which provides 10, 15, 20, and 25 purchase points respectively.").Default("").Short('p').String()
+		number     = pathfinder.Flag("number", "Number of characters to roll. Must be greater than 0.").Default("1").Short('n').Uint32()
+		verbose    = pathfinder.Flag("verbose", "Display additional information.").Default("false").Short('v').Bool()
 	)
 	kingpin.MustParse(pathfinder.Parse(os.Args[1:]))
 	if (*classic && *standard) ||
 		(*classic && *heroic) ||
 		(*classic && (*pool != "")) ||
-		(*classic && (*purchaseType != "")) ||
+		(*classic && (*purchase != "")) ||
 		(*standard && *heroic) ||
 		(*standard && (*pool != "")) ||
-		(*standard && (*purchaseType != "")) ||
+		(*standard && (*purchase != "")) ||
 		(*heroic && (*pool != "")) ||
-		(*heroic && (*purchaseType != "")) ||
-		((*pool != "") && (*purchaseType != "")) {
+		(*heroic && (*purchase != "")) ||
+		((*pool != "") && (*purchase != "")) {
 		pathfinder.FatalUsage("multiple generation types cannot be defined.\n")
 	}
-	// if *keep > *dice {
-	// 	pathfinder.FatalUsage("keep (%v) must be <= dice (%v).\n", *keep, *dice)
-	// }
-	// match, _ := regexp.MatchString(pathfinderpkg.DieModifierRegex, *modifier)
-	// if !match {
-	// 	pathfinder.FatalUsage("modifier (%v) is invalid.\n", *modifier)
-	// }
+	if *pool != "" && !pathfinderpkg.IsValidPoolSpecification(*pool) {
+		pathfinder.FatalUsage("invalid pool specification.\n")
+	}
+	if *purchase != "" && !pathfinderpkg.IsValidPurchaseSpecification(*purchase) {
+		pathfinder.FatalUsage("invalid purchase type specification.\n")
+	}
 
-	test := GenerationTypeClassic
-	return &test, nil, number, verbose
+	var gentype pathfinderpkg.GenerationType
+	var options string
+
+	switch {
+	case *classic:
+		gentype = pathfinderpkg.GenerationTypeClassic
+		options = ""
+	case *standard:
+		gentype = pathfinderpkg.GenerationTypeStandard
+		options = ""
+	case *heroic:
+		gentype = pathfinderpkg.GenerationTypeHeroic
+		options = ""
+	case *pool != "":
+		gentype = pathfinderpkg.GenerationTypePool
+		options = *pool
+	case *purchase != "":
+		gentype = pathfinderpkg.GenerationTypePurchase
+		options = *purchase
+	default:
+		gentype = pathfinderpkg.GenerationTypeStandard
+		options = ""
+	}
+	return gentype, options, *number, *verbose
 }
